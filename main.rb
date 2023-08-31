fbd24db5782f34f099c10cff48fcf5ef36ec8935
Thor <102772895+thorcamara@users.noreply.github.com>
2023-08-30 21:34:30 -0300

    Commit .

---
+++ main.rb
@@ -0,0 +1,44 @@
+require "./manager"
+
+def submenu(manager, to)
+  coin_list = manager.coin_list
+  puts "Avaiable Coins"
+  puts coin_list.join(", ").center(50, "-")
+  print "Coin: "
+  symbol = gets.chomp.upcase
+  print "Amount: "
+  amount = gets.chomp.to_i
+
+  if coin_list.include? symbol
+    result = manager.calculate(amount, symbol, to)
+    puts "-".center(50, "-")
+    puts "#{amount} #{symbol} = #{result} #{to}".center(50, "-")
+    puts "-".center(50, "-")
+  else
+    puts "#{symbol} is not avaiable".center(50, "*")
+  end
+end
+    
+
+def menu
+  manager = Manager.new
+  loop do
+    puts " Crypto Currency Converter ".center(50, "#")
+    puts "a) Convert to USD"
+    puts "b) Convert to EUR"
+    puts "q) Quit"
+    print "Action: "
+    choice = gets.chomp
+
+    case choice
+    when 'a' then submenu(manager, "USD") 
+    when 'b' then submenu(manager, "EUR") 
+    end
+
+    break unless choice != 'q'
+  end
+
+end
+
+
+menu()
---
+++ manager.rb
@@ -0,0 +1,36 @@
+require "uri"
+require "net/http"
+require "json"
+require "./coin"
+
+class Manager
+  @@repo = {}
+  def initialize
+    initialize_repo
+  end
+
+  def initialize_repo
+    response = web_scrap
+    json = JSON.parse(response)
+    for symbol, values in json
+      coin = Coin.new(symbol, values['USD'], values['EUR'])
+      @@repo[symbol] = coin
+    end
+  end
+
+  def web_scrap
+    url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,DASH,LTC&tsyms=USD,EUR"
+    uri = URI(url)
+    Net::HTTP.get(uri)
+  end
+
+  def coin_list
+    @@repo.keys
+  end
+
+  def calculate(amount, symbol, to)
+    coin = @@repo[symbol]
+    amount*coin.send(to.to_sym)
+  end
+
+end
